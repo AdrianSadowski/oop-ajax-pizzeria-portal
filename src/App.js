fe4c22a5fdea3ddd2c0762fecd94ddec3e3370fd
AdrianSadowski <sadow.adrian@gmail.com>
2021-11-27 17:04:04 +0100

    fix husky

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
